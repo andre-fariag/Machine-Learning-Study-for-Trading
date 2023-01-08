f2cc1f8eee951c0acdab3614e0b4fe68443146b4
andre-fariag <andrefaria-1@hotmail.com>
2023-01-08 19:40:17 -0300

    Plot data functions

---
+++ plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+#plot entire data frame
+def plot_data(df,title,x_label,y_label):
+    ax = df.plot(title=title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    plt.show()
+
+#plot a specficic region of data frame
+def plot_selected(df,columns,start_index,end_index,title,x_label,y_label):
+    plot_data(df.loc[start_index:end_index,columns],title,x_label,y_label)
