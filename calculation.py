754e15f318129647fdc7ac6697cf4e29a1b05ee1
andre-fariag <andrefaria-1@hotmail.com>
2023-01-09 09:54:24 -0300

    Daily and Cumulative returns

--- calculation.py
+++ calculation.py
@@ -13,3 +13,17 @@ def get_bollinger_bands(mean,std):
     upper_band = mean + std * 2
     lower_band = mean - std * 2
     return upper_band, lower_band
+
+#Calculates daily returns -> (price_today/price_yesterday) - 1
+def compute_daily_returns(df):
+    df_daily_returns = df.copy()
+    df_daily_returns = (df / df.shift(1)) - 1
+    df_daily_returns.iloc[0,:] = 0 #for row 0 there is no daily return -> prior price doesn't exist
+    return df_daily_returns
+
+#Calculates cumulative returns - longar time frame
+def compute_cumulative_returns(df):
+    df_cmlative_returns = df.copy()
+    df_cmlative_returns = (df / df_cmlative_returns.iloc[0,:]) - 1
+    df_cmlative_returns.iloc[0,:] = 0
+    return df_cmlative_returns
