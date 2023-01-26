8df55f6bd86e811b1c06c54eee804ca9b06b28a6
andre-fariag <andrefaria-1@hotmail.com>
2023-01-25 23:14:57 -0300

    plot fitted line on scatter plot and correlation of compared stocks

--- scatter.py
+++ scatter.py
@@ -1,6 +1,6 @@
 #scatterplot of daily returns from stocks
-#if alpha is grater then zero -> stock is on avarage performing better then another
-#beta is related to the slope of line fitting scatter plot -> concept of average rate between each stock
+#if alpha is grater then zero -> stock is on avarage performing better then another vs IBOV
+#beta is related to the slope of line fitting scatter plot -> concept of average rate between stock vs IBOV
 #correlation -> how tight the dots on scatter plots are
 
 import pandas as pd
@@ -24,6 +24,7 @@ def scatter_plot():
     #scatter plot IBOV x VALE3
     df_daily_return.plot(kind='scatter',x='IBOV',y='VALE3')
     beta_VALE3, alpha_VALE3 = np.polyfit(df_daily_return['IBOV'],df_daily_return['VALE3'],1)
+    plt.plot(df_daily_return['IBOV'], beta_VALE3*df_daily_return['IBOV'] + alpha_VALE3,'-',color='r')
     print("Beta VALE3: ",beta_VALE3)
     print("Alpha VALE3: ",alpha_VALE3)
     plt.grid()
@@ -31,11 +32,15 @@ def scatter_plot():
     #scatter plot IBOV x BBDC4
     df_daily_return.plot(kind='scatter',x='IBOV',y='BBDC4')
     beta_BBDC4, alpha_BBDC4 = np.polyfit(df_daily_return['IBOV'],df_daily_return['BBDC4'],1)
+    plt.plot(df_daily_return['IBOV'], beta_BBDC4*df_daily_return['IBOV'] + alpha_BBDC4,'-',color='r')
     print("Beta BBDC4: ",beta_BBDC4)
     print("Alpha BBDC4: ",alpha_BBDC4)
     plt.grid()
     
     plt.show()
+
+    #print correlation of dataframe
+    print (df_daily_return.corr(method='pearson'))
 
 if __name__ == "__main__":
     scatter_plot()
